e1f4f652f11a4787d5dc51f8fd971343a75c88e7
Jose Trinidad Garcia <internick32@gmail.com>
2022-10-10 08:58:34 -0500

    Banked de bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,18 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('Perjam'));
+bands.addBand(new Band('The Doors'));
 
 io.on('connection', client => {
 
     console.log('Cliente conectado...');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => { 
         console.log('Cliente desconectado...');
@@ -10,7 +20,31 @@ io.on('connection', client => {
 
     client.on('mensaje', (payload) => {
         console.log('Mensaje', payload);
-        io.emit('mensaje', {admin: 'Nuevo Mensaje'});
+        //io.emit('mensaje', {admin: 'Nuevo Mensaje'});
     });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    // client.on('emitir-mensaje', (payload) => {
+    //     console.log(payload);
+    //     io.emit('nuevo-mensaje', payload);
+    //     client.broadcast.emit('nuevo-mensaje', payload);
+    // });
+
+
 
 });
